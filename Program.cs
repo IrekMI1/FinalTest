f0b740bcfe6b49f9325d71b2379f41f13ba3b4d1
Irek <general157@mail.ru>
2022-09-28 18:49:04 +0300

    add solution in Program.cs

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Итоговая проверочная работа. 
+
+string[] CheckArrayElement(string[] arrayString)
+{
+    string[] checkedArray = new string[arrayString.Length];
+    for (int i = 0; i < arrayString.Length; i++)
+    {
+        if (arrayString[i].Length <= 3) checkedArray[i] = arrayString[i];
+    }
+    return checkedArray.Where(x => x != null).ToArray();
+}
+
+void PrintStringArray(string[] array)
+{
+    Console.Write("[");
+    if (array.Length != 0)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+            else Console.WriteLine($"{array[i]}]");
+        }
+    }
+    else Console.Write("]");
+}
+
+try
+{
+    Console.Write("Введите массив строк через ', ':\t");
+    string[] stringArray = Console.ReadLine().Split(", ").ToArray();
+    Console.Write("Результат:\t");
+    PrintStringArray(CheckArrayElement(stringArray));
+}
+catch 
+{
+    Console.WriteLine("Ошибка ввода!");
+}
